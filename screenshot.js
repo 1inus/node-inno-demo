e7a85c0393c7445bc7b84c892aa5967af8b65b89
1inus <895925636@qq.com>
2017-12-21 20:09:00 +0800

    save

---
+++ screenshot.js
@@ -0,0 +1,28 @@
+let path = require("path");
+let { spawn } = require('child_process');
+
+let exe = path.join(__dirname, "screenShot.exe");
+
+let cp = spawn(exe);
+
+cp.stdout.on('data', (data)=>{
+	console.log(data);
+});
+cp.stderr.on('data', (data)=>{
+	console.error(data);
+});
+
+cp.on('close', code => {
+	console.log("截图成功");
+	isCapturing = false
+
+	if (code !== 0) {
+		console.error(code);
+	} else {
+		console.log(code);
+	}
+});
+
+cp.on('error', err => {
+	console.error(err);
+});
